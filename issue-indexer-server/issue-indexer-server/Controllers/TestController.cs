d3737648918259599d578b5589e2d6c4e91b9cc9
justinsaji <justinsaji1@gmail.com>
2020-07-06 00:12:57 +1000

    Redirects to api on startup

---
+++ issue-indexer-server/issue-indexer-server/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace issue_indexer_server.Controllers {
+
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase {
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get() {
+            return "Up and running!";
+        }
+    }
+}
--- issue-indexer-server/issue-indexer-server/HealthCheck.cs
+++
@@ -1,7 +0,0 @@
-
-namespace issue_indexer_server {
-
-    public class HealthCheck {
-        public string Message { get; set; }
-    }
-}
